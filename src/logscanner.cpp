1e14945736c0b53408b5b7e2f3004b76a1b6ebf7
Slawek Ligus <root@ooz.ie>
2015-04-13 00:27:31 -0700

    Add inotify skeleton for a directory.

--- src/logscanner.cpp
+++ src/logscanner.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
+
+#include "FileScanner.h"
 
 using namespace std;
 
-int main() {
-	cout << "LogScanner" << endl;
+int main(int argc, char **argv) {
+
+	// TODO: Set up logging.
+	cerr << "LogScanner prototype" << endl;
+	// TODO: check if a directory.
+	// TODO: Parse args.
+	FileScanner linescanner(argv[1]);
+	// TODO: Extract config.
+	linescanner.scan();
 	return 0;
 }
